f9aa9ab7711e2f284a562afe382e3ee3abec2aeb
jdtogni <82490094+jdtogni78@users.noreply.github.com>
2022-01-15 01:25:36 -0800

    First unit test

--- app1/ModelChanges.sh
+++ app1/ModelChanges.sh
@@ -4,7 +4,25 @@ for f in $(ls \
         coreui-generator/app/Repositories/*Repository.php \
         coreui-generator/database/Factories/*Factory.php); do
     for c (Account Portfolio Fund Transaction Asset); do
-        sed -i.bkp -e "s/\(App\\Models\\${c}\)\(\[ ;\]\)/\1Ext\2/g" -e "s/${c}::class/${c}Ext::class/g" $f 
+        sed -i.bkp -e "s/\(App\\Models\\${c}\)\([ ;]\)/\1Ext\2/g" -e "s/${c}::class/${c}Ext::class/g" $f 
         rm $f.bkp
     done
+done
+
+# do not mess with auto generated fields from db
+# do not set random value for foreign keys
+for f in $(ls \
+        coreui-generator/database/Factories/*Factory.php); do
+    for c (deleted_at created_at updated_at); do
+        sed -i.bkp -e "s/\([']$c\)/\/\/\1/" $f
+        rm $f.bkp
+    done
+    sed -i.bkp -e "s/\(['].*_id[']\)/\/\/\1/" $f
+    rm $f.bkp
+done
+
+for f in $(ls \
+        coreui-generator/app/Models/*.php); do
+    sed -i.bkp "s/'created_at' => 'required'/'created_at' => 'nullable'/" $f
+    rm $f.bkp
 done

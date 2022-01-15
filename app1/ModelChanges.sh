ea0fdbff2d1aa94809bdbcc9506a117bb1fb5324
jdtogni <82490094+jdtogni78@users.noreply.github.com>
2022-01-14 22:47:51 -0800

    Created AsOf methods

--- app1/ModelChanges.sh
+++ app1/ModelChanges.sh
@@ -3,7 +3,8 @@ for f in $(ls \
         coreui-generator/app/Models/*.php \
         coreui-generator/app/Repositories/*Repository.php \
         coreui-generator/database/Factories/*Factory.php); do
-    for c (Account Portfolio Fund Transaction); do
-        echo sed -e "s/\(App\\Models\\${c}\)\(\[ ;\]\)/\1Ext\2/g" -e "s/${c}::class/${c}Ext::class/g" $f 
+    for c (Account Portfolio Fund Transaction Asset); do
+        sed -i.bkp -e "s/\(App\\Models\\${c}\)\(\[ ;\]\)/\1Ext\2/g" -e "s/${c}::class/${c}Ext::class/g" $f 
+        rm $f.bkp
     done
 done

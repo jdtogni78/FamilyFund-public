5b8b104ab3f4106c7b77efaa9f76797bfaef8038
jdtogni <82490094+jdtogni78@users.noreply.github.com>
2022-01-15 17:14:05 -0800

    Updating golden data

---
+++ database/drop_all.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `transactions`;
+DROP TABLE IF EXISTS `account_balances`;
+DROP TABLE IF EXISTS `account_matching_rules`;
+DROP TABLE IF EXISTS `account_trading_rules`;
+DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `asset_change_logs`;
+DROP TABLE IF EXISTS `asset_prices`;
+DROP TABLE IF EXISTS `assets`;
+DROP TABLE IF EXISTS `failed_jobs`;
+DROP TABLE IF EXISTS `funds`;
+DROP TABLE IF EXISTS `matching_rules`;
+DROP TABLE IF EXISTS `migrations`;
+DROP TABLE IF EXISTS `password_resets`;
+DROP TABLE IF EXISTS `personal_access_tokens`;
+DROP TABLE IF EXISTS `portfolio_assets`;
+DROP TABLE IF EXISTS `portfolios`;
+DROP TABLE IF EXISTS `trading_rules`;
+DROP TABLE IF EXISTS `transactions`;
+

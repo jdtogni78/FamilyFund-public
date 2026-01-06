508f27674b0e8c27b98e0f143faef27d4fdddb3e
jdtogni <82490094+jdtogni78@users.noreply.github.com>
2026-01-06 11:11:34 -0300

    Revert "Show trade portfolio graphs by default"
    This reverts commit 7fef7f3eca463e4c43707b69c2bc1ccd163102d7.

--- app1/family-fund-app/resources/views/trade_portfolios/inner_show_graphs.blade.php
+++ app1/family-fund-app/resources/views/trade_portfolios/inner_show_graphs.blade.php
@@ -1,20 +1,30 @@
-<div class="col-lg-6 mb-4 mb-lg-0">
-    <div class="card h-100">
+<div class="col">
+    <div class="card">
         <div class="card-header">
-            <strong><i class="fa fa-crosshairs me-2"></i>Trade Portfolio Target % {{ $extraTitle ?? '' }}</strong>
+            <a class="btn btn-primary" data-toggle="collapse" href="#collapseTPTA{{ $tradePortfolio->id }}" 
+            role="button" aria-expanded="false" aria-controls="collapseTPTA{{ $tradePortfolio->id }}">
+            Trade Portfolio Target %  {{ $extraTitle ?? '' }}
+            </a>
         </div>
-        <div class="card-body">
-            @include('trade_portfolios.graph')
+        <div class="collapse" id="collapseTPTA{{ $tradePortfolio->id }}">
+            <div class="card-body">
+                @include('trade_portfolios.graph')
+            </div>
         </div>
     </div>
 </div>
-<div class="col-lg-6">
-    <div class="card h-100">
+<div class="col">
+    <div class="card">
         <div class="card-header">
-            <strong><i class="fa fa-object-group me-2"></i>Trade Portfolio Group % {{ $extraTitle ?? '' }}</strong>
+            <a class="btn btn-primary" data-toggle="collapse" href="#collapseTPGB{{ $tradePortfolio->id }}" 
+            role="button" aria-expanded="false" aria-controls="collapseTPGB{{ $tradePortfolio->id }}">
+            Trade Portfolio Group %  {{ $extraTitle ?? '' }}
+            </a>
         </div>
-        <div class="card-body">
-            @include('trade_portfolios.group_graph')
+        <div class="collapse" id="collapseTPGB{{ $tradePortfolio->id }}">
+            <div class="card-body">
+                @include('trade_portfolios.group_graph')
+            </div>
         </div>
     </div>
 </div>
